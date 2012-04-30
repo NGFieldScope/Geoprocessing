27dd352d5e5d1246ed3bba62dadbc3d26135c19d
Eric Russell <erussell@ngs.org>
2012-04-30 11:03:30 -0400

    Only load data from the GSOD dataset, using two datasets was unnecessarily complicated Be smarter about situations when there's not yet enough data available to create a raster

--- gdd2/Scripts/gdd.py
+++ gdd2/Scripts/gdd.py
@@ -1,31 +1,9 @@
-import arcpy, csv, datetime, httplib, io, logging, os, re, sqlite3, sys, urllib
+import arcpy, csv, datetime, httplib, io, json, logging, os, re, sqlite3, sys, urllib
 from arcpy import env, sa
 
 _DBCONN = None
 _MOSAIC = 'growing_degree_days'
-_YESTERDAY = None
 logger = logging.getLogger('gdd')
-
-def create_temperature_database (path, stations_db):
-    '''Create an sqlite database for storing temperature station data. Load the station
-id and location information from the stations.db file, which should be distributed with
-this script'''
-    dbconn = sqlite3.connect(stations_db)
-    dbcurs = dbconn.cursor()
-    dbcurs.execute('SELECT * FROM station WHERE 1=1')
-    stations = list(dbcurs.fetchall())
-    dbcurs.close()
-    dbconn = sqlite3.connect(path)
-    dbcurs = dbconn.cursor()
-    dbcurs.execute('CREATE TABLE station (id VARCHAR(11) NOT NULL, source VARCHAR(5), name VARCHAR(64), easting INT NOT NULL, northing INT NOT NULL, elevation DOUBLE(6,1), gsod_daily BOOLEAN, PRIMARY KEY (id));')
-    dbcurs.execute('CREATE INDEX gsod_daily_index ON station (gsod_daily);')
-    dbcurs.execute('CREATE TABLE temperature (station VARCHAR(11) NOT NULL, tmin INT NOT NULL, tmax INT NOT NULL, date DATE NOT NULL, PRIMARY KEY (station,date));')
-    dbcurs.execute('CREATE INDEX temperature_station_index ON temperature (station);')
-    dbcurs.execute('CREATE INDEX temperature_date_index ON temperature (date);')
-    dbconn.commit()
-    dbcurs.executemany('INSERT INTO station (id,source,name,easting,northing,elevation,gsod_daily) VALUES (?, ?, ?, ?, ?, ?, ?)', stations)
-    dbconn.commit()
-    dbcurs.close()
 
 def setup_environment():
     '''Set up the complete execution environment for this script'''
@@ -61,99 +39,87 @@ def setup_environment():
         arcpy.AddField_management(_MOSAIC, 'BeginDate', 'DATE')
         arcpy.AddField_management(_MOSAIC, 'EndDate', 'DATE')
     # Create an sqlite database to hold the temperature station data, and open a connection to it
-    stations_db = os.path.join(data_folder, 'stations.db')
     temperature_db = os.path.join(scratch_folder, 'temperature.db')
     if not os.path.exists(temperature_db):
         logger.debug('creating temperature.db')
-        create_temperature_database(temperature_db, stations_db)
+        create_temperature_database(temperature_db)
     global _DBCONN
     _DBCONN = sqlite3.connect(temperature_db)
 
-def get_daily_data ():
-    '''Download temperature data from NOAA's Climate Prediction Center. Returns a tuple of the date and the data.'''
-    result = []
-    data = urllib.urlopen('http://www.cpc.ncep.noaa.gov/products/analysis_monitoring/cdus/prcp_temp_tables/dly_glob1.txt')
-    for i, row in enumerate(data):
-        if i == 21:
-            date = datetime.datetime.strptime(row.strip(), '%Y%m%d').date()
-            continue
-        if i < 22: continue # skip headers
-        tmax = int(row[0:4])
-        if tmax == -999: continue # skip missing data
-        tmin = int(row[4:8])
-        if tmin == -999: continue
-        id = row[28:33]
-        result.append((id, tmax, tmin, date,))
-    logger.debug('loaded %s points for %s from cpc.ncep.noaa.gov', len(result), date.isoformat())
-    return (date, result,)
+def create_temperature_database (path):
+    '''Create an sqlite database for storing temperature station data. Load the station
+id and location information from the NCDC's ArcGIS Server REST web service'''
+    logger.debug('creating database file and tables')
+    db_conn = sqlite3.connect(path)
+    db_cursor = db_conn.cursor()
+    db_cursor.execute('CREATE TABLE station (id VARCHAR(11) NOT NULL, name VARCHAR(64), x INT NOT NULL, y INT NOT NULL, PRIMARY KEY (id));')
+    db_cursor.execute('CREATE TABLE temperature (station VARCHAR(11) NOT NULL REFERENCES station(id), tmin INT NOT NULL, tmax INT NOT NULL, date DATE NOT NULL, PRIMARY KEY (station,date));')
+    db_cursor.execute('CREATE INDEX temperature_station_index ON temperature (station);')
+    db_cursor.execute('CREATE INDEX temperature_date_index ON temperature (date);')
+    db_conn.commit()
+    day_before_yesterday = datetime.date.today() - datetime.timedelta(2)
+    logger.debug('loading list of station ids from web service')
+    params = { 'where' : "COUNTRY='US' AND END_DATE>='%s'" % day_before_yesterday.strftime('%Y/%m/%d'),
+               'returnIdsOnly': 'true',
+               'f': 'json' }
+    response = read_post_response('gis.ncdc.noaa.gov', '/rest/services/cdo/gsod/MapServer/0/query', params)
+    objectids = json.loads(response)['objectIds']
+    stations = []
+    start_index = 0
+    logger.debug('loading data for %s stations' % len(objectids))
+    while start_index < len(objectids):
+        end_index = min(start_index + 200, len(objectids))
+        params = { 'where' : 'OBJECTID IN (%s)' % ','.join(map(str, objectids[start_index:end_index])),
+                   'outSR': '102100',
+                   'outFields': 'AWSBAN,STATION',
+                   'f': 'json' }
+        response = read_post_response('gis.ncdc.noaa.gov', '/rest/services/cdo/gsod/MapServer/0/query', params)
+        for record in json.loads(response)['features']:
+            stations.append((str(record['attributes']['AWSBAN']),
+                             str(record['attributes']['STATION']),
+                             int(record['geometry']['x']),
+                             int(record['geometry']['y']),))
+        start_index += 200
+    logger.debug('inserting stations into database')
+    db_cursor.executemany('INSERT INTO station (id,name,x,y) VALUES (?, ?, ?, ?)', stations)
+    db_conn.commit()
+    db_cursor.close()
 
-def get_gsod_data (begin_date, end_date, stations=None):
+def store_temperatures (begin_date, end_date):
     '''Download temperature data from National Climate Data Center's Global Summary of Day dataset'''
-    if stations is None:
-        cursor = _DBCONN.cursor()
-        cursor.execute("SELECT s.id FROM station s WHERE s.source='GSOD'")
-        stations = [ record[0] for record in cursor.fetchall() ]
-        cursor.close()
-    param_dict = { 'p_ndatasetid' : 10, 'datasetabbv' : 'GSOD', 'p_cqueryby' : 'ENTIRE',
-                   'p_csubqueryby' : '', 'p_nrgnid' : '', 'p_ncntryid' : '', 'p_nstprovid' : '',
-                   'volume' : 0, 'datequerytype' : 'RANGE', 'outform' : 'COMMADEL', 
-                   'startYear' : begin_date.year,
-                   'startMonth' : '%02d' % begin_date.month,
-                   'startDay' : '%02d' % begin_date.day,
-                   'endYear' : end_date.year,
-                   'endMonth' : '%02d' % end_date.month,
-                   'endDay' : '%02d' % end_date.day,
-                   'p_asubqueryitems' : stations }
-    params = urllib.urlencode(param_dict, True)
-    headers = {"Content-type": "application/x-www-form-urlencoded"}
-    conn = httplib.HTTPConnection('www7.ncdc.noaa.gov')
-    conn.request('POST', '/CDO/cdodata.cmd', params, headers)
-    response = conn.getresponse()
-    result = []
-    if response.status == 200:
-        result_page = response.read()
-        match = re.search('<p><a href="(http://www\d\.ncdc\.noaa\.gov/pub/orders/CDO\d+\.txt)">CDO\d+\.txt</a></p>', result_page, re.MULTILINE)
-        data = urllib.urlopen(match.group(1))
-        for i, row in enumerate(csv.reader(data)):
-            if i == 0: continue # skip headers
-            tmax = int(round(float(row[17][:-1])))
-            if tmax == 10000: continue
-            tmin = int(round(float(row[18][:-1])))
-            if tmin == 10000: continue
-            id = row[0] + row[1]
-            date = datetime.datetime.strptime(row[2].strip(), '%Y%m%d').date()
-            result.append((id, tmax, tmin, date,))
-        logger.debug('loaded %s points for %s to %s from ncdc.noaa.gov', len(result), begin_date.isoformat(), end_date.isoformat())
-    else:
-        logger.error('http response code %s from download request to ncdc.noaa.gov', response.status)
-    return result
+    logger.debug('loading data from %s to %s from ncdc.noaa.gov', begin_date.isoformat(), end_date.isoformat())
+    db_cursor = _DBCONN.cursor()
+    db_cursor.execute("SELECT s.id FROM station s")
+    stations = [ record[0] for record in db_cursor.fetchall() ]
+    params = { 'p_ndatasetid' : 10, 'datasetabbv' : 'GSOD', 'p_cqueryby' : 'ENTIRE',
+               'p_csubqueryby' : '', 'p_nrgnid' : '', 'p_ncntryid' : '', 'p_nstprovid' : '',
+               'volume' : 0, 'datequerytype' : 'RANGE', 'outform' : 'COMMADEL', 
+               'startYear' : begin_date.year,
+               'startMonth' : '%02d' % begin_date.month,
+               'startDay' : '%02d' % begin_date.day,
+               'endYear' : end_date.year,
+               'endMonth' : '%02d' % end_date.month,
+               'endDay' : '%02d' % end_date.day,
+               'p_asubqueryitems' : stations }
+    result_page = read_post_response('www7.ncdc.noaa.gov', '/CDO/cdodata.cmd', params)
+    records = []
+    match = re.search('<p><a href="(http://www\d\.ncdc\.noaa\.gov/pub/orders/CDO\d+\.txt)">CDO\d+\.txt</a></p>', result_page, re.MULTILINE)
+    csv_data = urllib.urlopen(match.group(1))
+    for i, row in enumerate(csv.reader(csv_data)):
+        if i == 0: continue # skip headers
+        tmax = int(round(float(row[17][:-1])))
+        if tmax == 10000: continue
+        tmin = int(round(float(row[18][:-1])))
+        if tmin == 10000: continue
+        id = row[0] + row[1]
+        date = datetime.datetime.strptime(row[2].strip(), '%Y%m%d').date()
+        records.append((id, tmax, tmin, date,))
+    logger.debug('loaded %s observations', len(records))
+    db_cursor.executemany('REPLACE INTO temperature (station,tmax,tmin,date) VALUES (?, ?, ?, ?)', records)
+    _DBCONN.commit()
+    db_cursor.close()
 
-def store_temperatures (date):
-    '''Store temperatures for the given date in the sqlite database. Uses CPC 
-and GSOD data if the requested day is available from the CPC, otherwise uses 
-only GSOD data using additional stations to replace the CPC data'''
-    sql = _DBCONN.cursor()
-    sql.execute('SELECT COUNT(*) FROM temperature t WHERE t.date=?', (date,))
-    tcount = sql.fetchall()[0][0]
-    if tcount == 0:
-        logger.debug('downloading data for %s', date.isoformat())
-        global _YESTERDAY
-        ddate = None
-        if _YESTERDAY is None or date == _YESTERDAY:
-            ddate, data = get_daily_data()
-            if _YESTERDAY is None:
-                _YESTERDAY = ddate
-        if ddate == date:
-            sql.execute("SELECT id FROM station WHERE gsod_daily=1")
-            gsod_stations = [ record[0] for record in sql.fetchall() ]
-            data.extend(get_gsod_data(date, date, gsod_stations))
-        else:
-            data = get_gsod_data(date, date)
-        sql.executemany('INSERT INTO temperature (station,tmax,tmin,date) VALUES (?, ?, ?, ?)', data)
-        _DBCONN.commit()
-    sql.close()
-
-def create_gdd_raster (date):
+def create_gdd_raster (date, min_temp, max_temp):
     '''Create a raster of growing degree days for the given date. Assumes
 that temperature data for that date has already been loaded into the
 database'''
@@ -161,28 +127,30 @@ database'''
     feature_class = arcpy.CreateFeatureclass_management("in_memory", "temp", "POINT")
     arcpy.AddField_management(feature_class, 'tmin', 'SHORT')
     arcpy.AddField_management(feature_class, 'tmax', 'SHORT')
-    cursor = arcpy.InsertCursor(feature_class)
+    fc_cursor = arcpy.InsertCursor(feature_class)
     point = arcpy.Point()
-    sql = _DBCONN.cursor()
-    sql.execute('SELECT s.easting,s.northing,t.tmax,t.tmin FROM temperature t INNER JOIN station s ON s.id=t.station WHERE t.date=?', (date,))
+    db_cursor = _DBCONN.cursor()
+    db_cursor.execute('SELECT s.x,s.y,t.tmax,t.tmin FROM temperature t INNER JOIN station s ON s.id=t.station WHERE t.date=?', (date,))
     rcount = 0
-    for record in sql.fetchall():
+    for record in db_cursor.fetchall():
         point.X = record[0]
         point.Y = record[1]
-        row = cursor.newRow()
+        row = fc_cursor.newRow()
         row.shape = point
         row.tmax = record[2]
         row.tmin = record[3]
-        cursor.insertRow(row)
+        fc_cursor.insertRow(row)
         rcount += 1
-    del cursor
+    db_cursor.close()
+    del fc_cursor
     logger.debug('interpolating %s points', rcount)
     arcpy.CheckOutExtension("Spatial")
     tmax_ras = sa.Idw(feature_class, 'tmax', 5000, 2, sa.RadiusVariable(10, 300000))
     tmin_ras = sa.Idw(feature_class, 'tmin', 5000, 2, sa.RadiusVariable(10, 300000))
-    gdd_ras = sa.Minus(sa.Divide(sa.Plus(tmax_ras, tmin_ras), 2), 50)
+    temp_range = max_temp - min_temp
+    gdd_ras = sa.Minus(sa.Divide(sa.Plus(tmax_ras, tmin_ras), 2), min_temp)
     gdd_ras = sa.Con(gdd_ras < 0, 0, gdd_ras)
-    gdd_ras = sa.Con(gdd_ras > 36, 36, gdd_ras)
+    gdd_ras = sa.Con(gdd_ras > temp_range, temp_range, gdd_ras)
     prev_day = date - datetime.timedelta(1)
     prev_ras = prev_day.strftime('GDD_%Y%m%d')
     if arcpy.Exists(prev_ras) and (date.month != 1 or date.day != 1):
@@ -215,27 +183,55 @@ raster catalog, and mark it as beloning to that date'''
         rows.updateRow(row)
     del rows
 
+def read_post_response (host, path, param_dict):
+    params = urllib.urlencode(param_dict, True)
+    headers = {"Content-type": "application/x-www-form-urlencoded"}
+    conn = httplib.HTTPConnection(host)
+    conn.request('POST', path, params, headers)
+    response = conn.getresponse()
+    if response.status == 200:
+        return response.read()
+    else:
+        raise httplib.HTTPException()
+
 def main (argv=None):
     '''Usage: <script> <begin_date(optional)> <end_date(optional)>
 create growing degree day rasters for each day between begin_date 
-(which defaults to yesterday) and end_date (which defaults to 
-begin_date), inclusive. Dates should be given in YYYY-MM-DD format.'''
+(which defaults to seven days ago) and end_date (which defaults to 
+today), inclusive. Dates should be given in YYYY-MM-DD format. Will
+only create rasters for days that don't already have one, and only
+if at least 1500 temperature observations are available.'''
     setup_environment()
-    begin_date = datetime.date.today() - datetime.timedelta(1)
+    begin_date = datetime.date.today() - datetime.timedelta(5)
+    end_date = datetime.date.today()
     if argv is not None and len(argv) > 0:
         begin_date = datetime.datetime.strptime(argv[0], '%Y-%m-%d').date()
-    end_date = begin_date
     if argv is not None and len(argv) > 1:
         end_date = datetime.datetime.strptime(argv[1], '%Y-%m-%d').date()
     if end_date < begin_date:
-        sys.stderr.write("begin_date must be before end_date\n")
-        return -1
-    date = begin_date
-    while date <= end_date:
-        store_temperatures(date)
-        raster = create_gdd_raster(date)
-        add_gdd_raster_to_mosaic(raster, date)
-        date = date + datetime.timedelta(1)
+        raise Exception('begin_date must be before end_date')
+    if begin_date.month != 1 or begin_date.day != 1:
+        prev_date = begin_date - datetime.timedelta(1)
+        if not arcpy.Exists(prev_date.strftime('GDD_%Y%m%d')):
+            raise Exception('beginning date %s is not the first day of year and previous day has no data' % begin_date.isoformat())
+    logger.info('running gdd script for %s to %s' % (begin_date.isoformat(), end_date.isoformat()))
+    while arcpy.Exists(begin_date.strftime('GDD_%Y%m%d')) and begin_date <= end_date:
+        logger.debug('raster already exists for %s' % begin_date)
+        begin_date = begin_date + datetime.timedelta(1)
+    if begin_date > end_date:
+        return 0
+    store_temperatures(begin_date, end_date)
+    db_cursor = _DBCONN.cursor()
+    current_date = begin_date
+    while current_date <= end_date:
+        db_cursor.execute('SELECT COUNT (*) from temperature t WHERE t.date=?', (current_date,))
+        if db_cursor.fetchone()[0] < 1500:
+            logger.debug('insufficient data to create raster for %s' % current_date)
+            break
+        raster = create_gdd_raster(current_date, 50, 86)
+        add_gdd_raster_to_mosaic(raster, current_date)
+        current_date = current_date + datetime.timedelta(1)
+    db_cursor.close()
     logger.debug('updating mosaic statistics')
     arcpy.CalculateStatistics_management(_MOSAIC)
     arcpy.BuildPyramidsandStatistics_management(_MOSAIC, 'INCLUDE_SUBDIRECTORIES', 'BUILD_PYRAMIDS', 'CALCULATE_STATISTICS')
@@ -244,5 +240,5 @@ begin_date), inclusive. Dates should be given in YYYY-MM-DD format.'''
 
 if __name__ == "__main__":
     status = main(sys.argv[1:])
-    sys.exit(status)
+    sys.exit(0)
     
